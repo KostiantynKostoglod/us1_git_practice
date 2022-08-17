00fc5ff960a01ac4d693b6fbe4ea8855c787ef51
Kostiantyn Kostoglod <0675053366@ukr.net>
2022-08-17 19:56:40 -0400

    2 HelloWorld.java is created

---
+++ src/day1/HelloWorld.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("good old days ");
+        System.out.println("haha 1");
+
+
+
+    }
+}
