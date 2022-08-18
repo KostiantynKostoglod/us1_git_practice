ce3d46668396bcb8e11fd0bea52d8b9a2e1eca55
Kostiantyn Kostoglod <0675053366@ukr.net>
2022-08-17 22:26:25 -0400

    8.3 Drink instructions updated, behaviour checked

--- src/howToBehave/Drink.java
+++ src/howToBehave/Drink.java
@@ -2,4 +2,7 @@ package howToBehave;
 
 public class Drink {
     // drink more then you can and vomit
+    //do not make any pauses after vomiting
+
+    // drink again at once
 }
--- src/howToBehave/Undress.java
+++ src/howToBehave/Undress.java
@@ -3,5 +3,7 @@ package howToBehave;
 public class Undress {
     public static void main(String[] args) {
         //take off you cloth
+
+        // try to seduce every first object
     }
 }
