37128b8accb0b89f3701272593ab50cc3fc84195
Kostiantyn Kostoglod <0675053366@ukr.net>
2022-08-17 21:07:33 -0400

    5 Login page is updated

--- src/day2/Login.java
+++ src/day2/Login.java
@@ -3,7 +3,11 @@ package day2;
 public class Login {
     public static void main(String[] args) {
 
+// page is updated after it has been created
 
+        // I am working on login page
+
+        // I am about to complete
 
 
     }
